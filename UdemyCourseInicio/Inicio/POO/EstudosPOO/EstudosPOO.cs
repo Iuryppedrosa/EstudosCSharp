ce5e3fbf488ddcdc05121f5dcbaa9776d6cf8a51
Iury Pedrosa <iuryppedrosa@protonmail.com>
2024-09-25 09:30:20 -0300

    Estudando construtores padrao, definido e com this, e metodos com this.

---
+++ UdemyCourseInicio/Inicio/POO/EstudosPOO/EstudosPOO.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCourseInicio.Inicio.POO.Models;
+
+namespace UdemyCourseInicio.Inicio.POO.EstudosPOO
+{
+    class EstudosPOO
+    {
+        // se nao houver um construtor, o C# cria um construtor vazio
+        // se omitir a palavra public, o C# entende que é privado
+        public void CriandoPessoa()
+        {
+
+            Pessoa pessoa = new Pessoa();
+            pessoa.nome = "João";
+            pessoa.idade = 25;
+            pessoa.altura = 1.80;
+            pessoa.sexo = "Masculino";
+
+            var carro = CriandoCarro();
+
+            Console.WriteLine("Nome: " + pessoa.nome);
+            Console.WriteLine("Idade: " + pessoa.idade);
+            Console.WriteLine("Altura: " + pessoa.altura);
+            Console.WriteLine("Sexo: " + pessoa.sexo);
+            Console.WriteLine("Multiplicador de idade: " + MultiplicadorDeIdade(2, pessoa.idade));
+            DonoDoCarro(carro, pessoa);
+        }
+
+        public Carro CriandoCarro()
+        {
+            Carro carro = new()
+            {
+                marca = "Fiat",
+                modelo = "Uno",
+                ano = 2022,
+                cor = "Vermelho"
+            };
+
+            Console.WriteLine("Marca: " + carro.marca);
+            Console.WriteLine("Modelo: " + carro.modelo);
+            Console.WriteLine("Ano: " + carro.ano);
+            Console.WriteLine("Cor: " + carro.cor);
+            return carro;
+        }
+
+        public int MultiplicadorDeIdade(int multiplicador, int idade)
+        {
+            return idade * multiplicador;
+        }
+
+        public void DonoDoCarro(Carro carro, Pessoa pessoa)
+        {
+            Console.WriteLine($"A pessoa {pessoa.nome} tem o carro {carro.modelo}");
+
+        }
+
+        public Aluno criandoAluno()
+        {
+            Aluno aluno = new Aluno("Maria", 20, 1.70, "Feminino", "Aprovado");
+            Console.WriteLine("-------Exibindo Aluno -------------");
+            Console.WriteLine("Nome: " + aluno.Nome);
+            Console.WriteLine("Idade: " + aluno.Idade);
+            Console.WriteLine("Altura: " + aluno.Altura);
+            Console.WriteLine("Sexo: " + aluno.Sexo);
+            Console.WriteLine("Aprovado: " + aluno.Aprovado);
+            return aluno;
+        }
+
+        public void CriandoAluno2()
+        {
+            Aluno aluno2 = new("Josefa");
+            Console.WriteLine("-------Exibindo Aluno 2-------------");
+            Console.WriteLine("Nome: " + aluno2.Nome);
+            if (aluno2.Idade == 0)
+            {
+                Console.WriteLine("Idade não informada");
+            }
+            if (aluno2.Altura == 0)
+            {
+                Console.WriteLine("Altura não informada");
+            }
+            if (aluno2.Sexo == null)
+            {
+                Console.WriteLine("Sexo não informado");
+            }
+            if (aluno2.Aprovado == null)
+            {
+                Console.WriteLine("Aprovado não informado");
+            }
+        }
+
+        public void CriandoCliente()
+        {
+            Cliente cl = new Cliente("Maria", 20, 1.70, "Feminino", "123.456.789-00");
+            Console.WriteLine("-------Exibindo Cliente -------------");
+            Console.WriteLine($"Nome: {cl.Nome}\nIdade: {cl.Idade}\nAltura: {cl.Altura}\nSexo: {cl.Sexo}\nCpf: {cl.Cpf}");
+
+            Cliente cl2 = new Cliente("Josefa", 25, 1.80, "Feminino", "987.654.321-00");
+            Console.WriteLine("-------Exibindo Cliente 2-------------");
+            cl2.Exibindo();
+        }
+
+    }
+}
+
---
+++ UdemyCourseInicio/Inicio/POO/Models/Pessoa.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCourseInicio.Inicio.POO.Models
+{
+    class Pessoa
+    {
+        public string? nome;
+        public int idade;
+        public double altura;
+        public string? sexo;
+
+    }
+
+    class Carro
+    {
+        public string? marca;
+        public string? modelo;
+        public int ano;
+        public string? cor;
+    }
+
+    class Aluno
+    {
+        public string? Nome;
+        public int Idade;
+        public double Altura;
+        public string? Sexo;
+        public string? Aprovado;
+
+        public Aluno(string nome)
+        {
+            Nome = nome;
+
+        }
+
+        public Aluno(string nome, int idade, double altura, string sexo, string aprovado) : this(nome)
+        {
+            Idade = idade;
+            Altura = altura;
+            Sexo = sexo;
+            Aprovado = aprovado;
+        }
+    }
+
+    public class Cliente
+    {
+        public string? Nome;
+        public int Idade;
+        public double Altura;
+        public string? Sexo;
+        public string? Cpf;
+
+        public Cliente(string Nome)
+        {
+            this.Nome = Nome;
+        }
+
+        public Cliente(string Nome, int Idade, double Altura, string Sexo, string Cpf) : this(Nome)
+        {
+            this.Idade = Idade;
+            this.Altura = Altura;
+            this.Sexo = Sexo;
+            this.Cpf = Cpf;
+        }
+
+        public void ExibindoComThis(Cliente cl)
+        {
+            Console.WriteLine("Nome2: " + cl.Nome);
+            Console.WriteLine("Idade2: " + cl.Idade);
+            Console.WriteLine("Altura2: " + cl.Altura);
+            Console.WriteLine("Sexo2: " + cl.Sexo);
+            Console.WriteLine("Cpf2: " + cl.Cpf);
+        }
+        public void Exibindo()
+        {
+            ExibindoComThis(this);
+        }
+    }
+}
